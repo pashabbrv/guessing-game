f9cd0c08db64bb3e5d4a0bb76716815b29c4d624
pashabbrv <pavelbobrofff@gmail.com>
2024-10-25 18:31:46 +0300

    guessing game class update

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentGuess = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.currentGuess = Math.floor((min + max) / 2);
     }
 
     guess() {
-
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess;
+        this.currentGuess = Math.floor((this.min + this.max) / 2);
     }
 
     greater() {
-
+        this.min = this.currentGuess;
+        this.currentGuess = Math.floor((this.min + this.max) / 2);
     }
 }
 
